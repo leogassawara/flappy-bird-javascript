1c6c53161c9e1c79f0b955ac8c6596915a73c338
Leonardo Ogassawara <leogassawara@hotmail.com>
2020-06-08 21:01:36 -0300

    Adicionando Plano de fundo e chão no jogo

--- jogo.js
+++ jogo.js
@@ -1,1 +1,91 @@
- 
+const sprites = new Image();
+sprites.src = "./sprites.png";
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+//[PLano de fundo]
+const planoDeFundo = {
+	spriteX: 390,
+	spriteY: 0,
+	largura: 275,
+	altura: 204,
+	x: 0,
+	y: canvas.height - 204,
+	desenha() {
+		contexto.fillStyle = '#70c5ce';
+		contexto.fillRect(0, 0, canvas.width, canvas.height)
+
+		contexto.drawImage(
+			sprites,
+			planoDeFundo.spriteX, planoDeFundo.spriteY,
+			planoDeFundo.largura, planoDeFundo.altura,
+			planoDeFundo.x, planoDeFundo.y,
+			planoDeFundo.largura, planoDeFundo.altura,
+		);
+		contexto.drawImage(
+			sprites,
+			planoDeFundo.spriteX, planoDeFundo.spriteY,
+			planoDeFundo.largura, planoDeFundo.altura,
+			(planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+			planoDeFundo.largura, planoDeFundo.altura,
+		);
+	}
+}
+
+//[Chão]
+const chao = {
+	spriteX: 0,
+	spriteY: 610,
+	largura: 224,
+	altura: 122,
+	x: 0,
+	y: canvas.height -112,
+	desenha() {
+		contexto.drawImage(
+			sprites,
+			chao.spriteX, chao.spriteY,
+			chao.largura, chao.altura,
+			chao.x, chao.y,
+			chao.largura, chao.altura,
+		);
+
+		contexto.drawImage(
+			sprites,
+			chao.spriteX, chao.spriteY,
+			chao.largura, chao.altura,
+			(chao.x + chao.largura), chao.y,
+			chao.largura, chao.altura,
+		);
+	}
+}
+
+//[Bird]
+const flappybird = {
+	spriteX: 0,
+	spriteY: 0,
+	largura: 33,
+	altura: 24,
+	x: 10,
+	y: 50,
+	desenha() {
+		contexto.drawImage(
+			sprites, 
+			flappybird.spriteX, flappybird.spriteY,
+			flappybird.largura, flappybird.altura,
+			flappybird.x, flappybird.y,
+			flappybird.largura, flappybird.altura,
+		);
+	}
+}
+
+function loop() {
+	planoDeFundo.desenha();
+	chao.desenha();
+	flappybird.desenha();
+	
+	
+	requestAnimationFrame(loop);
+};
+
+loop();
